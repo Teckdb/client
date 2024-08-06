271c860116ae1071044c9de292fde286d270d87d
Erickhub32 <erick.arevalolopez25@gmail.com>
2024-08-06 17:30:21 +0200

    EditCoffeeForm updated, wikicoffee on progress

--- src/pages/WikiCoffeePage/WikiCoffeePage.jsx
+++ src/pages/WikiCoffeePage/WikiCoffeePage.jsx
@@ -1,15 +1,92 @@
 import "./WikiCoffePage.css"
-import { CardBody, Col, Container, Row } from "react-bootstrap"
+import { CardBody, Col, Container, Row, Card, ListGroup, Image, Ratio } from "react-bootstrap"
+
 
 
 const WikiCoffeePage = () => {
     return (
-        <Container fluid className="bg-secondary m-0 flex-grow-1 text-white d-flex justify-content-center align-items-center">
+        <Container>
             <Row>
-                <Col xxl>
-                    <h1>Soy WikiCoffee</h1>
+
+                <Col md={{ span: 3 }}>
+                    <Card className="mb-5">
+                        <Card.Img variant="top" src="src/assets/roaster-at-local-coffee-business.jpg" />
+                        <Card.Body>
+                            <Card.Title>Card Title</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the
+                                bulk of the card's content.
+                            </Card.Text>
+                        </Card.Body>
+                        <ListGroup className="list-group-flush">
+                            <ListGroup.Item>Cras justo odio</ListGroup.Item>
+                            <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+                            <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+                        </ListGroup>
+                        <Card.Body>
+                            <Card.Link href="#">Card Link</Card.Link>
+                            <Card.Link href="#">Another Link</Card.Link>
+                        </Card.Body>
+                    </Card>
+                </Col>
+
+                <Col md={{ span: 6 }}>
+                    <h1>Tips</h1>
+                    <Image src="src/assets/lenses-lattes.jpg" thumbnail />
+                    <Ratio aspectRatio="16x9">
+                        <iframe
+                            src="src/assets/CAFE CLANDESTINO.mov"
+                            title="YouTube video"
+                            allowFullScreen>
+                        </iframe>
+                    </Ratio>
+                </Col>
+
+                <Col md={{ span: 3 }}>
+                    <Card className="mb-5">
+                        <Card.Img variant="top" src="src/assets/coffee-press-into-crystal.jpg" />
+                        <Card.Body>
+                            <Card.Title>Card Title</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the
+                                bulk of the card's content.
+                            </Card.Text>
+                        </Card.Body>
+                        <ListGroup className="list-group-flush">
+                            <ListGroup.Item>Cras justo odio</ListGroup.Item>
+                            <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+                            <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+                        </ListGroup>
+                        <Card.Body>
+                            <Card.Link href="#">Card Link</Card.Link>
+                            <Card.Link href="#">Another Link</Card.Link>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col md={{ span: 4 }}>
+                    <Card className="mb-5">
+                        <Card.Img variant="top" src="src/assets/freshly-picked-coffee-beans.jpg" />
+                        <Card.Body>
+                            <Card.Title>Card Title</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the
+                                bulk of the card's content.
+                            </Card.Text>
+                        </Card.Body>
+                        <ListGroup className="list-group-flush">
+                            <ListGroup.Item>Cras justo odio</ListGroup.Item>
+                            <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+                            <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+                        </ListGroup>
+                        <Card.Body>
+                            <Card.Link href="#">Card Link</Card.Link>
+                            <Card.Link href="#">Another Link</Card.Link>
+                        </Card.Body>
+                    </Card>
                 </Col>
             </Row>
+
+
         </Container>
     )
 }
