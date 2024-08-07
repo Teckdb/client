28932b34eddfefc33d98b3288b129ca2dfac8090
Erickhub32 <erick.arevalolopez25@gmail.com>
2024-08-07 18:18:15 +0200

    uodated

--- src/components/WikiCoffee/WikiCoffee/WikiCoffee.jsx
+++ src/components/WikiCoffee/WikiCoffee/WikiCoffee.jsx
@@ -6,8 +6,28 @@ const WikiCoffee = () => {
     return (
         <Container fluid className="bg-secondary m-0 flex-grow-1 text-white d-flex justify-content-center align-items-center">
             <Row>
-                <Col xxl>
-                    <h1>Commentarios</h1>
+                <Col md={{ span: 3 }}>
+                    <Stack direction='horizontal' gap={2}>
+                        <Badge>Curiosities about Coffee</Badge>
+                    </Stack>
+                    <Card className='mb-5'>
+                        <CardImg variant='top' src='src/assets/roaster-at-local-coffee-business.jpg'>
+                            <CardBody>
+                                <CardTitle>Coffee Roasting</CardTitle>
+                                <CardText>
+                                    Coffee roasting transforms green beans into aromatic, flavorful coffee through controlled heat application.
+                                </CardText>
+                            </CardBody>
+                            <ListGroup>
+                                <ListGroupItem>FairTrade</ListGroupItem>
+                            </ListGroup>
+                            <CardBody>
+                                <CardLink href='#'>See Article</CardLink>
+                            </CardBody>
+
+                        </CardImg>
+                    </Card>
+
                 </Col>
             </Row>
         </Container>
