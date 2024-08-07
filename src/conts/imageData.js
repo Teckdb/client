8a1a816670843337996842d4429e04c999be8919
Erickhub32 <erick.arevalolopez25@gmail.com>
2024-08-07 12:25:32 +0200

    working in home

---
+++ src/conts/imageData.js
@@ -0,0 +1,5 @@
+export const HERO_IMG_1 = 'https://res.cloudinary.com/dcz7y0fvi/image/upload/v1723020938/home-1-slider-image-1_yju3ym.jpg'
+export const HERO_IMG_2 = 'https://res.cloudinary.com/dcz7y0fvi/image/upload/v1723020938/home-1-slider-image-2_lnxiiq.jpg'
+export const HERO_IMG_3 = 'https://res.cloudinary.com/dcz7y0fvi/image/upload/v1723020938/home-1-slider-image-3_tf0kfk.jpg'
+export const LOGO_SMALL = 'https://res.cloudinary.com/dcz7y0fvi/image/upload/v1723020938/WHATEVER.jpg'
+
