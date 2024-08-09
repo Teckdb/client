c49868e3db08f7ba3225bcbc6624fd3f34bd871d
Erickhub32 <erick.arevalolopez25@gmail.com>
2024-08-09 10:41:08 +0200

    updated vite

--- src/conts/apiEndpoints.js
+++ src/conts/apiEndpoints.js
@@ -1,1 +1,1 @@
-export const API_URL = 'https://ironcoffeeloverserver.fly.dev'
+export const API_URL = import.meta.env.VITE_API_URL // esto, para este trabajo está bien, pero no se puede rpetir, a futuro tiene qu estar siempre en una constante en todas la páginas.
